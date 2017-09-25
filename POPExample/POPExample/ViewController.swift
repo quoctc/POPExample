ac87b330dc8d62f435d7a625a270e9e4fe8f8488
Quoc Tran <quoc.tran@saritasa.com>
2017-09-25 14:23:20 +0700

    Init project

---
+++ POPExample/POPExample/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  POPExample
+//
+//  Created by Quoc Tran on 9/25/17.
+//  Copyright © 2017 Quoc Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+
+    @IBOutlet weak var booksTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+//for tableview datasource and delegate
+extension ViewController {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "bookCell")
+        
+        return cell
+    }
+}
+
