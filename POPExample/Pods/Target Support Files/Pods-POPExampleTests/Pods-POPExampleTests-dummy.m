27ccde5a50740dc5fcd28c79506a3ac3bdc16b97
Quoc Tran <quoc.tran@saritasa.com>
2017-10-16 11:03:12 +0700

    Init pod, added firebase

---
+++ POPExample/Pods/Target Support Files/Pods-POPExampleTests/Pods-POPExampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_POPExampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_POPExampleTests
+@end
---
+++ POPExample/Pods/Target Support Files/Pods-POPExampleUITests/Pods-POPExampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_POPExampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_POPExampleUITests
+@end
